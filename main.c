2324539b9553a9604c8e7a8f2e32ee20e773df28
GrimmFantasy <GrimmFantasy@users.noreply.github.com>
2020-11-18 10:28:45 +0000

    (Auto) Apply Linting

--- main.c
+++ main.c
@@ -14,9 +14,9 @@ int main(int argc, char *argv[])
     int seed;
     int *parSize;
     char *optionsSize;
- 
 
-   
+
+
     simConfig config;
 
     config.contactsRisk = 1;
@@ -35,80 +35,80 @@ int main(int argc, char *argv[])
     config.amountOfContacts = 5;
 
 
-    optionsSize = malloc(sizeof(char) * floor(argc/2));
-    parSize = malloc(sizeof(int) * floor(argc/2));
+    optionsSize = malloc(sizeof(char) * floor(argc / 2));
+    parSize = malloc(sizeof(int) * floor(argc / 2));
 
-    if (argc%2 ==0)
-    {
-        printf("ERROR: Invaild inputs detected.\nMake sure that every option is follow by a value.\n");
+    if (argc % 2 == 0) {
+        printf
+            ("ERROR: Invaild inputs detected.\nMake sure that every option is follow by a value.\n");
         return 0;
     }
-   
+
     /* indlaeser parametre */
     for (i = 0; i < argc; i++) {
         if (argv[i][0] == '-') {
-            *(optionsSize +j) = argv[i][1];
+            *(optionsSize + j) = argv[i][1];
             j++;
         } else if (isdigit(argv[i][0])) {
             *(parSize + k) = atoi(argv[i]);
             k++;
         }
-        
+
     }
-    
+
 
     /* Switch over command line options */
-    for (i = 0; i < floor(argc/2); i++) {
+    for (i = 0; i < floor(argc / 2); i++) {
         switch (*(optionsSize + i)) {
-        case 'z':/*how many angents have sympums when infected*/
-            config.symptomaticPercent = *(parSize +i);
+        case 'z':              /*how many angents have sympums when infected */
+            config.symptomaticPercent = *(parSize + i);
             break;
 
-        case 'w': /*chanc that angent will isolate*/
-            config.willIsolatePercent = *(parSize +i);
+        case 'w':              /*chanc that angent will isolate */
+            config.willIsolatePercent = *(parSize + i);
             break;
 
-        case 'c':/*risk of infetion*/
-            config.contactsRisk = *(parSize +i);
+        case 'c':              /*risk of infetion */
+            config.contactsRisk = *(parSize + i);
             break;
 
-        case 'k':/*amount of contacts pr agent*/
-            config.amountOfContacts = *(parSize +i);
+        case 'k':              /*amount of contacts pr agent */
+            config.amountOfContacts = *(parSize + i);
             break;
 
-        case 't':/*size of primary group*/
-            config.primaryGroupSize = *(parSize +i);
-            break;
-        
-        case 'y':/*size of secound group*/
-            config.primaryGroupSize = *(parSize +i);
+        case 't':              /*size of primary group */
+            config.primaryGroupSize = *(parSize + i);
             break;
 
-        case 'a':/*amount of time incted*/
-            config.infectionTime = *(parSize +i);
+        case 'y':              /*size of secound group */
+            config.primaryGroupSize = *(parSize + i);
             break;
 
-        case 'p':/*total amount of agents*/
-            config.amountOfAgents = *(parSize +i);
+        case 'a':              /*amount of time incted */
+            config.infectionTime = *(parSize + i);
             break;
 
-        case 'i':/*amount of infected at start of simulation*/
-            config.amountOfStartInfected = *(parSize +i);
+        case 'p':              /*total amount of agents */
+            config.amountOfAgents = *(parSize + i);
             break;
 
-        case 'e':/*lenght of simulation*/
-            config.maxEvents = *(parSize +i);
+        case 'i':              /*amount of infected at start of simulation */
+            config.amountOfStartInfected = *(parSize + i);
             break;
 
-        case 's':/*seed*/
-            config.seed = *(parSize +i);
+        case 'e':              /*lenght of simulation */
+            config.maxEvents = *(parSize + i);
+            break;
+
+        case 's':              /*seed */
+            config.seed = *(parSize + i);
             break;
         }
 
     }
     free(optionsSize);
     free(parSize);
-  
+
 
     config.amountOfPrimaryGroups =
         config.amountOfAgents / config.primaryGroupSize;
