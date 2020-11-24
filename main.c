85c45fc5696948ba61555788657c0616e975c4a4
GrimmFantasy <GrimmFantasy@users.noreply.github.com>
2020-11-24 10:35:39 +0000

    (Auto) Apply Linting

--- main.c
+++ main.c
@@ -36,59 +36,60 @@ int main(int argc, char *argv[])
         if (argv[i][0] == '-') {
 
             if (argv[i][1] != 'g' && !isdigit(argv[i + 1][0])) {
-            printf
-                ("ERROR: Invaild inputs detected.\nMake sure that every option is follow by a value.\nRunning simulation with user perameters op to %c.\nAlle other inputs will be defualt perameters.\n", argv[i][1]);
-            break;
+                printf
+                    ("ERROR: Invaild inputs detected.\nMake sure that every option is follow by a value.\nRunning simulation with user perameters op to %c.\nAlle other inputs will be defualt perameters.\n",
+                     argv[i][1]);
+                break;
             }
-            value = atoi(argv[i+1]);
+            value = atoi(argv[i + 1]);
             switch (argv[i][1]) {
-                case 'z':          /*how many angents have sympums when infected */
-                    config.symptomaticPercent = value;
-                    break;
+            case 'z':          /*how many angents have sympums when infected */
+                config.symptomaticPercent = value;
+                break;
 
-                case 'w':          /*chanc that angent will isolate */
-                    config.willIsolatePercent = value;
-                    break;
+            case 'w':          /*chanc that angent will isolate */
+                config.willIsolatePercent = value;
+                break;
 
-                case 'c':          /*risk of infetion */
-                    config.contactsRisk = value;
-                    break;
+            case 'c':          /*risk of infetion */
+                config.contactsRisk = value;
+                break;
 
-                case 'k':          /*amount of contacts pr agent */
-                    config.amountOfContacts = value;
-                    break;
+            case 'k':          /*amount of contacts pr agent */
+                config.amountOfContacts = value;
+                break;
 
-                case 't':          /*size of primary group */
-                    config.groupSize[0] = value;
-                    break;
+            case 't':          /*size of primary group */
+                config.groupSize[0] = value;
+                break;
 
-                case 'y':          /*size of secound group */
-                    config.groupSize[1] = value;
-                    break;
+            case 'y':          /*size of secound group */
+                config.groupSize[1] = value;
+                break;
 
-                case 'a':          /*amount of time incted */
-                    config.infectionTime = value;
-                    break;
+            case 'a':          /*amount of time incted */
+                config.infectionTime = value;
+                break;
 
-                case 'p':          /*total amount of agents */
-                    config.amountOfAgents = value;
-                    break;
+            case 'p':          /*total amount of agents */
+                config.amountOfAgents = value;
+                break;
 
-                case 'i':          /*amount of infected at start of simulation */
-                    config.amountOfStartInfected = value;
-                    break;
+            case 'i':          /*amount of infected at start of simulation */
+                config.amountOfStartInfected = value;
+                break;
 
-                case 'e':          /*lenght of simulation */
-                    config.maxEvents = value;
-                    break;
+            case 'e':          /*lenght of simulation */
+                config.maxEvents = value;
+                break;
 
-                case 's':          /*seed */
-                    config.seed = value;
-                    break;
+            case 's':          /*seed */
+                config.seed = value;
+                break;
 
-                case 'g':
-                    graph = 1;
-                    break;
+            case 'g':
+                graph = 1;
+                break;
             }
         }
 
