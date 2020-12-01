4ba7bfea3fa1a1326915aaeb3b2c32ffbd7854b9
benjadahl <benjadahl@gmail.com>
2020-12-01 13:55:15 +0100

    Made main.c print again

--- main.c
+++ main.c
@@ -37,7 +37,7 @@ int main(int argc, char *argv[])
     config.partyMeetChance = 10;
     config.willTestPercent = 75;
     config.seed = 0;
-    config.print = 0;
+    config.print = 1;
     config.groupSize[0] = 15;
     config.groupSize[1] = 10;
     config.primaryGroupRisk = 5;
