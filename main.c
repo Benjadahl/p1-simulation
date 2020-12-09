026cb3c71f9b83744f55fda33ac606d64cfe9240
Casper <casper11andersen@gmail.com>
2020-12-09 15:10:21 +0100

    tilføjede kommentare

--- main.c
+++ main.c
@@ -115,16 +115,19 @@ int main(int argc, char *argv[])
 
     /* indlaeser parametre */
     for (i = 0; i < argc; i++) {
-
+        /*grunden til at vi har valgt at der skal være et '-' foran 
+        en parameterinstilling er for at gøre det nemmer at opsamle i vores program*/
         if (argv[i][0] == '-') {
-
+            /*grunden til at vi har denne if statment er fordi at både 'g' og 'b'
+            skal ikke efterføgles af en værdi, så for at sikre os at det inputs
+            som skal have en værdi tilknyttede til sig har det bruge vi dette*/
             if ((argv[i][1] != 'g' && argv[i][1] != 'b') && !isdigit(argv[i + 1][0])) {
                 printf
                     ("ERROR: Invaild inputs detected.\nMake sure that every option is follow by a value.\nInvaild argument %c\n",
                      argv[i][1]);
                 return EXIT_FAILURE;
             } else {
-
+                /*dette sikre os at den ikke prøver at få fat i en værdi som ikke eksistere*/
                 if (i + 1 < argc) {
                     value = atoi(argv[i + 1]);
                 }
