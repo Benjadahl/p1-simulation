9fb5bac0a3282b2e825b63086e061f77e45f121b
Mads Kristensen <madsmario@gmail.com>
2020-12-12 10:11:51 +0100

    Organising main.c

--- main.c
+++ main.c
@@ -8,6 +8,8 @@
 #include "simulation.h"
 #include "export.h"
 
+void initStandardConfig(simConfig *config);
+void initConfigWithInputParameters(simConfig *config, double value, char input, int *graph);
 void run_simulation(gsl_rng * r, simConfig config, DataSet * data,
                     int dataCount);
 void calculateAveragePlot(int run, int events, DataSet * data,
@@ -34,92 +36,8 @@ int main(int argc, char *argv[])
 
     simConfig config;
 
-    /*party */
-    config.partyChance = 16;
-    config.partyRisk = 0.15;
+    initStandardConfig(&config);
 
-    /*Groups */
-    config.primaryGroupRisk = 0.05; /*can */
-    config.secondaryGroupRisk = 0.025;  /*can */
-    config.contactsRisk = 0.03; /*can */
-    config.groupSize[0].lowerbound = 5;
-    config.groupSize[0].upperbound = 50;
-    config.groupSize[0].varians = 400;
-    config.groupSize[0].expectedValue =
-        (config.groupSize[0].lowerbound +
-         config.groupSize[0].upperbound) / 2;
-    config.groupSize[1].lowerbound = 5;
-    config.groupSize[1].upperbound = 30;
-    config.groupSize[1].varians = 100;
-    config.groupSize[1].expectedValue =
-        (config.groupSize[1].lowerbound +
-         config.groupSize[1].upperbound) / 2;
-    config.groupSize[2].lowerbound = 2;
-    config.groupSize[2].upperbound = 10;
-    config.groupSize[2].varians = 9;
-    config.groupSize[2].expectedValue = 5;
-    config.groupSize[3].upperbound = 50;
-    config.groupSize[3].lowerbound = 2;
-    config.groupSize[3].expectedValue = 6;
-    config.groupSize[3].varians = 9;
-    config.toMeet[0].upperbound = 10;
-    config.toMeet[0].lowerbound = 0;
-    config.toMeet[0].expectedValue = 5;
-    config.toMeet[0].varians = 9;
-    config.toMeet[1].upperbound = 20;
-    config.toMeet[1].lowerbound = 0;
-    config.toMeet[1].expectedValue = 3;
-    config.toMeet[1].varians = 9;
-    config.toMeet[2].upperbound = 3;
-    config.toMeet[2].lowerbound = 0;
-    config.toMeet[2].expectedValue = 1;
-    config.toMeet[2].varians = 2;
-    config.toMeet[3].upperbound = 15;
-    config.toMeet[3].lowerbound = 0;
-    config.toMeet[3].expectedValue = 3;
-    config.toMeet[3].varians = 9;
-    config.passerbys.lowerbound = 0;
-    config.passerbys.upperbound = 25;
-    config.passerbys.varians = 8;
-    config.passerbys.expectedValue = 4;
-
-    /*App */
-    config.chanceToHaveApp = 0; /*can */
-    config.btThreshold = 0;
-    config.btDecay = 7;
-
-    /*Infections */
-    config.infectionTime.lowerbound = 2;
-    config.infectionTime.upperbound = 12;
-    config.infectionTime.varians = 1;
-    config.infectionTime.expectedValue = 4;
-    config.amountOfStartInfected = 1;
-    config.symptomaticPercent = 0.84; /*can */
-    config.incubationTime.lowerbound = 1; /* CDC.gov */
-    config.incubationTime.upperbound = 14;  /* CDC.gov */
-    config.incubationTime.varians = 1;
-    config.incubationTime.expectedValue = 5.1;  /* CDC.gov *//*can */
-    config.isolationDelay.upperbound = 5;
-    config.isolationDelay.lowerbound = 0;
-    config.isolationDelay.expectedValue = 1;
-    config.isolationDelay.varians = 1;
-
-    /*Misc */
-    config.simulationRuns = 1;
-    config.maxEvents = 100;
-    config.seed = 0;
-    config.print = 1;
-    config.amountOfAgents = 100000;
-    config.willIsolatePercent = 0.9;  /*can */
-    config.willTestPercent = 0.6; /*can */
-    config.testResponseTime.upperbound = 3; /*can, this is a worst case */
-    config.testResponseTime.lowerbound = 1;
-    config.testResponseTime.expectedValue = 2;
-    config.testResponseTime.varians = 1;
-    config.chanceOfCorrectTest = 0.95;
-    config.passerByRisk = 0.0048;
-    config.makeConfigFile = 1;
-    config.dataLabel = 1;
 
     /* indlaeser parametre */
     for (i = 0; i < argc; i++) {
@@ -140,116 +58,9 @@ int main(int argc, char *argv[])
                 if (i + 1 < argc) {
                     value = strtod(argv[i + 1], &emPtr);
                 }
-
-                switch (argv[i][1]) {
-                case 'z':      /*how many angents have sympums when infected */
-                    config.symptomaticPercent =
-                        isValueCorrect(argv[i][1], value, 0, 1);
-                    break;
-
-                case 'w':      /*chanc that angent will isolate */
-                    config.willIsolatePercent =
-                        isValueCorrect(argv[i][1], value, 0, 1);
-                    break;
-
-                case 'c':      /*risk of infetion */
-                    config.contactsRisk =
-                        isValueCorrect(argv[i][1], value, 0, 1);
-                    break;
-
-                case 'k':      /*amount of contacts pr agent */
-                    config.groupSize[2].expectedValue = isValueCorrect
-                        (argv[i][1], value, config.groupSize[2].lowerbound,
-                         config.groupSize[2].upperbound);
-                    break;
-
-                case 't':      /*size of primary group */
-                    config.groupSize[0].expectedValue = isValueCorrect
-                        (argv[i][1], value, config.groupSize[0].lowerbound,
-                         config.groupSize[0].upperbound);
-                    break;
-
-                case 'y':      /*size of secound group */
-                    config.groupSize[1].expectedValue = isValueCorrect
-                        (argv[i][1], value, config.groupSize[1].lowerbound,
-                         config.groupSize[1].upperbound);
-                    break;
-
-                case 'a':      /*amount of time incted */
-                    config.infectionTime.expectedValue = isValueCorrect
-                        (argv[i][1], value,
-                         config.infectionTime.lowerbound,
-                         config.infectionTime.upperbound);
-                    break;
-
-                case 'p':      /*total amount of agents */
-                    config.amountOfAgents =
-                        isValueCorrect(argv[i][1], value, 1, INT_MAX);
-                    break;
-
-                case 'i':      /*amount of infected at start of simulation */
-                    config.amountOfStartInfected =
-                        isValueCorrect(argv[i][1], value, 1, INT_MAX);
-                    break;
-
-                case 'e':      /*lenght of simulation */
-                    config.maxEvents =
-                        isValueCorrect(argv[i][1], value, 0, INT_MAX);
-                    break;
-
-                case 's':      /*seed */
-                    config.seed =
-                        isValueCorrect(argv[i][1], value, 0, INT_MAX);
-                    break;
-
-                case 'g':
-                    graph = 1;
-                    break;
-
-                case 'b':
-                    config.groupSize[0].lowerbound = 2;
-                    config.groupSize[0].upperbound = 10;
-                    config.groupSize[0].varians = 6;
-                    config.groupSize[0].expectedValue = 5;
-                    config.groupSize[1].lowerbound = 2;
-                    config.groupSize[1].upperbound = 10;
-                    config.groupSize[1].varians = 6;
-                    config.groupSize[1].expectedValue = 5;
-                    config.groupSize[2].lowerbound = 0;
-                    config.groupSize[2].upperbound = 10;
-                    config.groupSize[2].varians = 9;
-                    config.groupSize[2].expectedValue = 5;
-                    config.groupSize[3].upperbound = 20;
-                    config.groupSize[3].lowerbound = 2;
-                    config.groupSize[3].expectedValue = 6;
-                    config.groupSize[3].varians = 9;
-                    break;
-
-                case 'd':
-                    config.chanceToHaveApp =
-                        isValueCorrect(argv[i][1], value, 0, 1);
-                    break;
-
-                case 'f':
-                    config.btThreshold =
-                        isValueCorrect(argv[i][1], value, 0, INT_MAX);
-                    break;
-
-                case 'h':
-                    config.btDecay =
-                        isValueCorrect(argv[i][1], value, 0, INT_MAX);
-                    break;
-
-                case 'j':
-                    config.simulationRuns =
-                        isValueCorrect(argv[i][1], value, 0, INT_MAX);
-                    break;
-
-                case 'l':
-                    config.dataLabel =
-                        isValueCorrect(argv[i][1], value, 0, INT_MAX);
-                }
             }
+
+            initConfigWithInputParameters(&config, value, argv[i][1], &graph);
         }
     }
 
@@ -313,6 +124,207 @@ int main(int argc, char *argv[])
         free(avgData[i].data);
     }
     return EXIT_SUCCESS;
+}
+
+void initStandardConfig(simConfig *config){
+        /*party */
+    config->partyChance = 16;
+    config->partyRisk = 0.15;
+
+    /*Groups */
+    config->primaryGroupRisk = 0.05; /*can */
+    config->secondaryGroupRisk = 0.025;  /*can */
+    config->contactsRisk = 0.03; /*can */
+    config->groupSize[0].lowerbound = 5;
+    config->groupSize[0].upperbound = 50;
+    config->groupSize[0].varians = 400;
+    config->groupSize[0].expectedValue =
+        (config->groupSize[0].lowerbound +
+         config->groupSize[0].upperbound) / 2;
+    config->groupSize[1].lowerbound = 5;
+    config->groupSize[1].upperbound = 30;
+    config->groupSize[1].varians = 100;
+    config->groupSize[1].expectedValue =
+        (config->groupSize[1].lowerbound +
+         config->groupSize[1].upperbound) / 2;
+    config->groupSize[2].lowerbound = 2;
+    config->groupSize[2].upperbound = 10;
+    config->groupSize[2].varians = 9;
+    config->groupSize[2].expectedValue = 5;
+    config->groupSize[3].upperbound = 50;
+    config->groupSize[3].lowerbound = 2;
+    config->groupSize[3].expectedValue = 6;
+    config->groupSize[3].varians = 9;
+    config->toMeet[0].upperbound = 10;
+    config->toMeet[0].lowerbound = 0;
+    config->toMeet[0].expectedValue = 5;
+    config->toMeet[0].varians = 9;
+    config->toMeet[1].upperbound = 20;
+    config->toMeet[1].lowerbound = 0;
+    config->toMeet[1].expectedValue = 3;
+    config->toMeet[1].varians = 9;
+    config->toMeet[2].upperbound = 3;
+    config->toMeet[2].lowerbound = 0;
+    config->toMeet[2].expectedValue = 1;
+    config->toMeet[2].varians = 2;
+    config->toMeet[3].upperbound = 15;
+    config->toMeet[3].lowerbound = 0;
+    config->toMeet[3].expectedValue = 3;
+    config->toMeet[3].varians = 9;
+    config->passerbys.lowerbound = 0;
+    config->passerbys.upperbound = 25;
+    config->passerbys.varians = 8;
+    config->passerbys.expectedValue = 4;
+
+    /*App */
+    config->chanceToHaveApp = 0; /*can */
+    config->btThreshold = 0;
+    config->btDecay = 7;
+
+    /*Infections */
+    config->infectionTime.lowerbound = 2;
+    config->infectionTime.upperbound = 12;
+    config->infectionTime.varians = 1;
+    config->infectionTime.expectedValue = 4;
+    config->amountOfStartInfected = 1;
+    config->symptomaticPercent = 0.84; /*can */
+    config->incubationTime.lowerbound = 1; /* CDC.gov */
+    config->incubationTime.upperbound = 14;  /* CDC.gov */
+    config->incubationTime.varians = 1;
+    config->incubationTime.expectedValue = 5.1;  /* CDC.gov *//*can */
+    config->isolationDelay.upperbound = 5;
+    config->isolationDelay.lowerbound = 0;
+    config->isolationDelay.expectedValue = 1;
+    config->isolationDelay.varians = 1;
+
+    /*Misc */
+    config->simulationRuns = 1;
+    config->maxEvents = 100;
+    config->seed = 0;
+    config->print = 1;
+    config->amountOfAgents = 100000;
+    config->willIsolatePercent = 0.9;  /*can */
+    config->willTestPercent = 0.6; /*can */
+    config->testResponseTime.upperbound = 3; /*can, this is a worst case */
+    config->testResponseTime.lowerbound = 1;
+    config->testResponseTime.expectedValue = 2;
+    config->testResponseTime.varians = 1;
+    config->chanceOfCorrectTest = 0.95;
+    config->passerByRisk = 0.0048;
+    config->makeConfigFile = 1;
+    config->dataLabel = 1;
+}
+
+void initConfigWithInputParameters(simConfig *config, double value, char input, int *graph){
+    switch (input) {
+        case 'z':      /*how many angents have sympums when infected */
+            config->symptomaticPercent =
+                isValueCorrect(input, value, 0, 1);
+            break;
+
+        case 'w':      /*chanc that angent will isolate */
+            config->willIsolatePercent =
+                isValueCorrect(input, value, 0, 1);
+            break;
+
+        case 'c':      /*risk of infetion */
+            config->contactsRisk =
+                isValueCorrect(input, value, 0, 1);
+            break;
+
+        case 'k':      /*amount of contacts pr agent */
+            config->groupSize[2].expectedValue = isValueCorrect
+                (input, value, config->groupSize[2].lowerbound,
+                 config->groupSize[2].upperbound);
+            break;
+
+        case 't':      /*size of primary group */
+            config->groupSize[0].expectedValue = isValueCorrect
+                (input, value, config->groupSize[0].lowerbound,
+                 config->groupSize[0].upperbound);
+            break;
+
+        case 'y':      /*size of secound group */
+            config->groupSize[1].expectedValue = isValueCorrect
+                (input, value, config->groupSize[1].lowerbound,
+                 config->groupSize[1].upperbound);
+            break;
+
+        case 'a':      /*amount of time incted */
+            config->infectionTime.expectedValue = isValueCorrect
+                (input, value,
+                 config->infectionTime.lowerbound,
+                 config->infectionTime.upperbound);
+            break;
+
+        case 'p':      /*total amount of agents */
+            config->amountOfAgents =
+                isValueCorrect(input, value, 1, INT_MAX);
+            break;
+
+        case 'i':      /*amount of infected at start of simulation */
+            config->amountOfStartInfected =
+                isValueCorrect(input, value, 1, INT_MAX);
+            break;
+
+        case 'e':      /*lenght of simulation */
+            config->maxEvents =
+                isValueCorrect(input, value, 0, INT_MAX);
+            break;
+
+        case 's':      /*seed */
+            config->seed =
+                isValueCorrect(input, value, 0, INT_MAX);
+            break;
+
+        case 'g':
+            *graph = 1;
+            break;
+
+        case 'b':
+            config->groupSize[0].lowerbound = 2;
+            config->groupSize[0].upperbound = 10;
+            config->groupSize[0].varians = 6;
+            config->groupSize[0].expectedValue = 5;
+            config->groupSize[1].lowerbound = 2;
+            config->groupSize[1].upperbound = 10;
+            config->groupSize[1].varians = 6;
+            config->groupSize[1].expectedValue = 5;
+            config->groupSize[2].lowerbound = 0;
+            config->groupSize[2].upperbound = 10;
+            config->groupSize[2].varians = 9;
+            config->groupSize[2].expectedValue = 5;
+            config->groupSize[3].upperbound = 20;
+            config->groupSize[3].lowerbound = 2;
+            config->groupSize[3].expectedValue = 6;
+            config->groupSize[3].varians = 9;
+            break;
+
+        case 'd':
+            config->chanceToHaveApp =
+                isValueCorrect(input, value, 0, 1);
+            break;
+
+        case 'f':
+            config->btThreshold =
+                isValueCorrect(input, value, 0, INT_MAX);
+            break;
+
+        case 'h':
+            config->btDecay =
+                isValueCorrect(input, value, 0, INT_MAX);
+            break;
+
+        case 'j':
+            config->simulationRuns =
+                isValueCorrect(input, value, 0, INT_MAX);
+            break;
+
+        case 'l':
+            config->dataLabel =
+                isValueCorrect(input, value, 0, INT_MAX);
+        }
+
 }
 
 double isValueCorrect(char input, double value, int min, int max)
